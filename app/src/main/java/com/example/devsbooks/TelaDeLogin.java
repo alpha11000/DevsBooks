8577c7d36101cb30df739878f41430da37607cfe
Wesley <wesleyvitor37417@gmail.com>
2021-10-30 17:41:44 -0300

    Adição dos efeitos ripple e selector aos botões, os quais serão determinado de acordo com a versão do android do usuário

--- app/src/main/java/com/example/devsbooks/TelaDeLogin.java
+++ app/src/main/java/com/example/devsbooks/TelaDeLogin.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class TelaDeLogin extends AppCompatActivity {
 
@@ -21,7 +24,20 @@ public class TelaDeLogin extends AppCompatActivity {
 
         user = findViewById(R.id.login_user_field);
         password = findViewById(R.id.login_password_field);
-        
+
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+
+                if( (actionId == EditorInfo.IME_ACTION_DONE)){
+                    autenticar();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
         findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
